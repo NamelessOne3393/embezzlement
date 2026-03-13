f1348bb2037b9371a26d33734aef6ea83dc4b7cf
harshlall92 <harsh.lall92@gmail.com>
2026-03-12 22:01:00 -0700

    Frame work for main Frame

--- src/main/java/com/cosc210/models/state/GameState.java
+++ src/main/java/com/cosc210/models/state/GameState.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import com.cosc210.models.GameProperties;
 import static com.cosc210.models.state.SaveState.loadGame;
 import static com.cosc210.models.state.SaveState.saveGame;
+import com.cosc210.ui.MainJFrame;
 import com.cosc210.ui.consoleDisplay;
 import com.cosc210.ui.inputHandler;
 
@@ -41,6 +42,7 @@ public class GameState{
             consoleDisplay.printMainMenu();
         }
         saveGame(propertiesList, Schilling,fileName);
+        MainJFrame mainMenu = new MainJFrame();
     }
 
     public static ArrayList<GameProperties> getPropertiesList() {
--- src/main/java/com/cosc210/ui/MainJFrame.java
+++ src/main/java/com/cosc210/ui/MainJFrame.java
@@ -1,8 +1,30 @@
 package com.cosc210.ui;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.*;
+import java.awt.*;
 
 public class MainJFrame extends JFrame implements ActionListener {
-    
+    private final int X_SIZE = 1000, Y_SIZE = 700;
+    private JLabel wallet, properties, buy, sell, exit, save, load;
+    private JTextField propertiesField;
+    public MainJFrame(){
+        super("Embezzlement");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(X_SIZE,Y_SIZE));
+        ((JPanel) getContentPane()).setBorder(new EmptyBorder(13, 13, 13, 13) );
+        setLayout(new FlowLayout());
+        JButton propertiesBtn = new JButton("Properties");
+        propertiesBtn.setActionCommand("List");
+        propertiesBtn.addActionListener(this);
+        properties = new JLabel("List of Properties");
+        //ICON
+        ImageIcon gameIcon = new ImageIcon();//Need to add image and then folder path(Relative)
+        this.setIconImage(gameIcon.getImage());
+        
+        //BACKGROUND COLOR or IMAGE
+        this.getContentPane().setBackground(new Color(173, 216, 230));
+    }
 }
