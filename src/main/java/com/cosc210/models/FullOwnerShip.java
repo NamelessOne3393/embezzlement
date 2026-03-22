59c7dce42b0c8d1685d34a5608f52a963020328e
name <keeganzemp7@gmail.com>
2026-03-21 22:49:09 -0700

    one last bugfix

--- src/main/java/com/cosc210/models/FullOwnerShip.java
+++ src/main/java/com/cosc210/models/FullOwnerShip.java
@@ -7,73 +7,78 @@ import com.cosc210.models.exception.notEnoughOwnershipException;
 
 /**
  * This class reprents the buyable object that need to bought as whole
- * It will keep track of Money Rate, Original Price, number of it owned, 
- *  and it name
+ * It will keep track of Money Rate, Original Price, number of it owned,
+ * and it name
  **/
-public class FullOwnerShip extends GameProperties{
+public class FullOwnerShip extends GameProperties {
 
-    //REQURES: propertiesPrice, numProperties > 0
-    //MODIFIES: this
-    //EFFECT: Makes FullOwnerShip have it attributes 
-    public FullOwnerShip(int propertiesPrice, int numProperties, String name){
-        this.propertiesPrice = propertiesPrice; 
+    // REQURES: propertiesPrice, numProperties > 0
+    // MODIFIES: this
+    // EFFECT: Makes FullOwnerShip have it attributes
+    public FullOwnerShip(int propertiesPrice, int numProperties, String name) {
+        this.propertiesPrice = propertiesPrice;
         this.numProperties = numProperties;
         this.name = name;
         moneyRate = numProperties * propertiesPrice;
     }
-    //REQURES: propertiesPrice, numProperties > 0
-    //MODIFIES: this
-    //EFFECT: Makes FullOwnerShip have it attributes 
-    public FullOwnerShip(JSONObject obj){
-        
+
+    // REQURES: propertiesPrice, numProperties > 0
+    // MODIFIES: this
+    // EFFECT: Makes FullOwnerShip have it attributes
+    public FullOwnerShip(JSONObject obj) {
+
         System.out.println(obj);
-        this.propertiesPrice = obj.getInt("propertiesPrice"); 
+        this.propertiesPrice = obj.getInt("propertiesPrice");
         this.numProperties = obj.getInt("numProperties");
         this.name = obj.getString("name");
         moneyRate = numProperties * propertiesPrice;
     }
-    //MODIFIES: this
-    //EFFECT: Increase numProperties and updates moneyRate and money
+
+    // MODIFIES: this
+    // EFFECT: Increase numProperties and updates moneyRate and money
     @Override
-    public void incOwn(int num) throws notEnoughMoneyException{
-        if (getMoney() < num *propertiesPrice){
+    public void incOwn(int num) throws notEnoughMoneyException {
+        if (getMoney() < num * propertiesPrice) {
             throw new notEnoughMoneyException();
-        }else{
-        numProperties+= num;
-        moneyRate = numProperties * propertiesPrice;
-        setMoney(-num*propertiesPrice);
+        } else {
+            numProperties += num;
+            moneyRate = numProperties * propertiesPrice;
+            setMoney(-num * propertiesPrice);
         }
     }
-    //REQURES: have enought to properties to sell 
-    //MODIFIES: this
-    //EFFECT: decreases numProperties by 1 and updates moneyRate and money
+
+    // REQURES: have enought to properties to sell
+    // MODIFIES: this
+    // EFFECT: decreases numProperties by 1 and updates moneyRate and money
     @Override
-    public void decOwn(int num) throws notEnoughOwnershipException{
-        if(numProperties <= 0){
+    public void decOwn(int num) throws notEnoughOwnershipException {
+        if (numProperties - num < 0) {
             throw new notEnoughOwnershipException("Not enough to sell");
         } else {
-            setMoney(num*propertiesPrice);
-            numProperties-= num;
+            setMoney(num * propertiesPrice);
+            numProperties -= num;
             moneyRate = numProperties * propertiesPrice;
         }
 
     }
-    //REQURES: have properties to sell 
-    //MODIFIES: this
-    //EFFECT: decreases numProperties to 0 and updates moneyRate and money
+
+    // REQURES: have properties to sell
+    // MODIFIES: this
+    // EFFECT: decreases numProperties to 0 and updates moneyRate and money
     @Override
-    public void sellAll()throws notEnoughOwnershipException{
+    public void sellAll() throws notEnoughOwnershipException {
         decOwn(numProperties);
     }
-    //EFFECT: Saves moneyRate, propertiesPrice, numProperties, and name     
+
+    // EFFECT: Saves moneyRate, propertiesPrice, numProperties, and name
     @Override
-    public JSONObject ExportData(){
+    public JSONObject ExportData() {
         JSONObject out = new JSONObject();
 
-        out.put("name",name);
-        out.put("propertiesPrice",propertiesPrice);
-        out.put("numProperties",numProperties);
-        out.put("type","full");
+        out.put("name", name);
+        out.put("propertiesPrice", propertiesPrice);
+        out.put("numProperties", numProperties);
+        out.put("type", "full");
 
         return out;
     }
